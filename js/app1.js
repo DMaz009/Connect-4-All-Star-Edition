d0ec5ed75fb40459f4656c6a728803c7b9b00e31
David Mazza <DavidMazza@Davids-MacBook-Pro.local>
2021-12-14 11:02:39 -0600

    Playing around with js in App1

---
+++ js/app1.js
@@ -0,0 +1,56 @@
+console.log("Connect-4");
+
+
+//Global variables//
+
+// let player1 = prompt('Player One: Enter your name. You will be Pink');
+// let player2 = prompt('Player Two: Enter your name. You will be Purple');
+let userName = "";
+let currentPlayer = 1;
+let slot;
+
+// let player1Color = 'pink';
+// let player2Color = 'purple';
+
+// Selectors //
+
+
+const tableRow = document.getElementsByTagName('tr')
+
+const tableSlots = document.querySelectorAll('td')
+
+const slots = document.querySelectorAll('.slots')
+
+const playerTurn = document.querySelector('.playerTurn')
+
+const reset = document.querySelector('.reset')
+
+
+
+// Class
+
+const Game {
+  constructor(playerColor, name) {
+    this.playerColor = playerColor,
+    this.currentPlayer = 1,
+    this.name = name
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// You have come to the end //
